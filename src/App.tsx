3f3faaa626af6866773936fdca3c738cddf57fcd
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-10 11:24:29 +0100

    Install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Movie Search</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
