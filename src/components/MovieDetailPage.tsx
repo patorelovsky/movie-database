4c430eaad7b340937802bf73d433059133555452
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-11 13:11:20 +0100

    Add loading and error component

--- src/components/MovieDetailPage.tsx
+++ src/components/MovieDetailPage.tsx
@@ -1,27 +1,42 @@
-import { LinearProgress, Paper } from "@mui/material";
+import { Alert, Box, LinearProgress, Paper } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MovieDetail, fetchMovieDetailApi } from "../api";
 
 export default function MovieDetailPage() {
+  const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<MovieDetail>();
+  const [error, setError] = useState<string>("");
   const { id } = useParams();
 
   useEffect(() => {
-    async function loadMovieDetail(movieId: string) {
-      setMovie(await fetchMovieDetailApi(movieId));
-    }
     if (id) {
-      loadMovieDetail(id);
+      fetchMovieDetailApi(id)
+        .then((movie) => setMovie(movie))
+        .catch((error: Error) => {
+          console.error(error);
+          setError(error.message);
+        })
+        .finally(() => setIsLoading(false));
+    } else {
+      setIsLoading(false);
     }
   }, [id]);
 
-  return movie ? (
-    <Paper sx={{ m: 2, p: 2 }}>
-      <h1>{movie.Title}</h1>
-      <img src={movie.Poster} alt="" />
-    </Paper>
-  ) : (
-    <LinearProgress />
+  return (
+    <Box>
+      {isLoading && <LinearProgress />}
+      {error && (
+        <Alert sx={{ m: 5 }} severity="error">
+          {error}
+        </Alert>
+      )}
+      {movie && (
+        <Paper sx={{ m: 2, p: 2 }}>
+          <h1>{movie.Title}</h1>
+          <img src={movie.Poster} alt="" />
+        </Paper>
+      )}
+    </Box>
   );
 }
