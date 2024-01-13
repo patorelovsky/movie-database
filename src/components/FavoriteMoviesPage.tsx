773e6e48eaa367abeaf88c0f4a8b0c31aafb12e4
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-13 21:39:14 +0100

    Create favorite movies page

--- src/components/FavoriteMoviesPage.tsx
+++ src/components/FavoriteMoviesPage.tsx
@@ -1,5 +1,24 @@
-import { Box } from "@mui/material";
+import { Alert, Box } from "@mui/material";
+import MovieSearchGrid from "./MovieSearchGrid";
+import { getFavoriteMovies, useAppDispatch, useAppSelector } from "../redux";
+import { useEffect } from "react";
 
 export default function FavoriteMoviesPage() {
-  return <Box></Box>;
+  const favMovies = useAppSelector(({ favoriteMovies }) => favoriteMovies);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getFavoriteMovies());
+  }, [dispatch]);
+
+  return (
+    <Box sx={{ m: 2 }}>
+      {favMovies.length > 0 ? (
+        <MovieSearchGrid movies={favMovies} />
+      ) : (
+        <Alert severity="info">
+          No favorite movies yet. Add some to your favorites!
+        </Alert>
+      )}
+    </Box>
+  );
 }
