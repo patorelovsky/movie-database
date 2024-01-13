f0f17fabd4fc3034313efc2510b4e0907b0fe9cb
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-13 17:15:02 +0100

    Add duplicate unit test

--- src/services/localStorageService.spec.ts
+++ src/services/localStorageService.spec.ts
@@ -6,10 +6,16 @@ import {
 import { SearchMovie } from ".";
 
 describe("favoriteMoviesApi", () => {
-  const testMovie: SearchMovie = {
+  const testMovie1: SearchMovie = {
     imdbID: "tt123456",
     Poster: "www.test.com/test.png",
     Title: "Test Movie",
+    Year: "2024",
+  };
+  const testMovie2: SearchMovie = {
+    imdbID: "tt789012",
+    Poster: "www.test.com/test.png",
+    Title: "Test Movie 2",
     Year: "2024",
   };
 
@@ -23,22 +29,37 @@ describe("favoriteMoviesApi", () => {
   });
 
   it("should add movie to favorite movies", () => {
-    addToFavoriteMovies(testMovie);
+    addToFavoriteMovies(testMovie1);
     const expected = getFavoriteMovies();
-    expect(expected).toEqual([testMovie]);
+    expect(expected).toEqual([testMovie1]);
+  });
+
+  it("should not add duplicate movie to favorite movies", () => {
+    addToFavoriteMovies(testMovie1);
+    // Adding the same movie again
+    addToFavoriteMovies(testMovie1);
+    const expected = getFavoriteMovies();
+    expect(expected).toEqual([testMovie1]);
+  });
+
+  it("should add a new movie alongside existing ones to favorite movies", () => {
+    addToFavoriteMovies(testMovie1);
+    addToFavoriteMovies(testMovie2);
+    const expected = getFavoriteMovies();
+    expect(expected).toEqual([testMovie1, testMovie2]);
   });
 
   it("should remove a movie from localStorage", () => {
-    addToFavoriteMovies(testMovie);
-    removeFromFavoriteMovies(testMovie.imdbID);
+    addToFavoriteMovies(testMovie1);
+    removeFromFavoriteMovies(testMovie1.imdbID);
     const storedMovies = getFavoriteMovies();
     expect(storedMovies).toEqual([]);
   });
 
   it("should handle non-existing movie ID", () => {
-    addToFavoriteMovies(testMovie);
+    addToFavoriteMovies(testMovie1);
     removeFromFavoriteMovies("nonexistentId");
     const storedMovies = getFavoriteMovies();
-    expect(storedMovies).toEqual([testMovie]);
+    expect(storedMovies).toEqual([testMovie1]);
   });
 });
